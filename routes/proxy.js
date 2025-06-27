3a8d81883283c39422a0b27b74a5f93e1df0e925
Waqas Munir <waqas.payza@gmail.com>
2025-06-28 00:54:17 +0500

    Updated Market Api

--- routes/proxy.js
+++ routes/proxy.js
@@ -9,7 +9,7 @@ router.get('/:id', async (req, res) => {
   try {
     const response = await axios.get(
       `https://api.coingecko.com/api/v3/coins/${id}/market_chart`,
-      { params: { vs_currency: 'usd', days: 1, interval: 'hourly' } }
+      { params: { vs_currency: 'usd', days: 1 } }
     );
     res.json(response.data);
   } catch (err) {
